6cdf14f7cbde1912bfd02e0ba38220d3b807165f
Marco Vit <not_defined@somewhere.it>
2015-01-10 19:59:40 +0100

    Getter and setter implementation

--- src/main/java/foo/bar/math/Polynomial.java
+++ src/main/java/foo/bar/math/Polynomial.java
@@ -1,23 +1,32 @@
 package foo.bar.math;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 public class Polynomial {
 
-	private final List<Double> coefficients = new ArrayList<Double>();
+	private final SortedMap<Integer, Double> coefficients = new TreeMap<Integer, Double>();
 	
 	
 	public int getDeegree() {
-		return this.coefficients.size();
+		int deegree = (this.coefficients.isEmpty()) ? 0 : this.coefficients.lastKey();  
+		return deegree;
 	}
 	
 	public void setCoefficent(int deegree, double coefficient) {
-		throw new UnsupportedOperationException("not yet implemented");
+		this.checkDeegreeParameter(deegree);
+		this.coefficients.put(deegree, coefficient);
 	}
 	
 	public double getCoefficent(int deegree) {
-		throw new UnsupportedOperationException("not yet implemented");
+		this.checkDeegreeParameter(deegree);
+		return this.coefficients.get(deegree);
+	}
+	
+	private void checkDeegreeParameter(int deegree) {
+		if(deegree < 0) {
+			throw new IllegalArgumentException("deegree must be 0 or positive");
+		}
 	}
 	
 	public double computeFor(double x) {
